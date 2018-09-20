3e351d19fb77d09057bb817b3efa617bddab513d
stillFly <15132167216@163.com>
2018-09-20 09:36:45 +0800

    Add files via upload

---
+++ exec.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def step_function(x):
+    return np.array(x>0, dtype=np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0,x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = step_function(x)
+y2 = sigmoid(x)
+plt.figure()
+plt.plot(x,y1)
+plt.plot(x,y2)
+plt.ylim(-0.1, 1.1)
+plt.show()
